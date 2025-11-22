859888042ace232f7fd681fa07c1765d7a33def1
_Real <underscorereal@atomicmail.io>
2025-11-22 18:44:45 +0200

    gay.example

--- Example.lua
+++ Example.lua
@@ -1,9 +1,9 @@
 -- New example script written by wally
 -- You can suggest changes with a pull request or something
 
-local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+local repo = 'https://raw.githubusercontent.com/underscoreReal/LinoriaLib/main/'
 
-local Library = loadstring(readfile('test-branch.lua'))()
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
